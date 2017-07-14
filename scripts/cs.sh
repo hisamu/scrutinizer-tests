b3b14a78889f0fe6ccc8774f55fa8039b0edaa1e
Luã de Souza <lua.desouza@theiconic.com.au>
2017-07-14 15:05:18 +1000

    test cs php

--- scripts/cs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo '<?xml version="1.0" encoding="UTF-8"?>
-<checkstyle version="1.0.0">
-    <file name="./src/Bar.php">
-        <error line="2"  message="Test" source="Ruleset.RuleName"/>
-    </file>
-</checkstyle>'
