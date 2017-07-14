5540e48a267cdf2939f4fe47e27fd856fe9bb2d1
Luã de Souza <lua.desouza@theiconic.com.au>
2017-07-14 15:04:38 +1000

    Finish cs

--- scripts/cs.php
+++ scripts/cs.php
@@ -2,13 +2,19 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Process\Process;
 
 const BASEPATH_COMMAND = 'git rev-parse --show-toplevel';
 const COMMITS_COMMAND = 'git log master..HEAD --pretty=tformat:"%H"';
 const FILES_COMMAND = 'git diff -M --name-only master^ HEAD -- *.php';
-const LINES_COMMAND = "git blame -p %s | awk '/%s/ {print $3,$4}'";
+const LINES_COMMAND = "git blame -p %s | awk '/^(%s)/ {print $3}'";
 const CS_COMMAND = '%s/vendor/bin/phpcs --standard=PSR2 %s --report=json';
+
+$output = new ConsoleOutput();
+$reports = [];
+$hasError = false;
 
 $runCommand = function (string $command): array {
     $process = new Process($command);
@@ -23,11 +29,51 @@ $commits = implode('|', $runCommand(COMMITS_COMMAND));
 
 foreach ($files as $file) {
     $filePath = sprintf('%s/%s', $basePath, $file);
+    $reports[$file] = $reports[$file] ?? [];
 
     $linesChanged = $runCommand(sprintf(LINES_COMMAND, $filePath, $commits));
-    $report = json_decode(current($runCommand(sprintf(CS_COMMAND, $basePath, $file))));
+    $report = json_decode(current($runCommand(sprintf(CS_COMMAND, $basePath, $file))), true);
 
-    echo "<pre>"; print_r ($linesChanged); die("</pre>");
+    $report = current($report['files']);
+    $messages = $report['messages'];
+
+    foreach ($messages as $message) {
+        if (!in_array($message['line'], $linesChanged)) {
+            continue;
+        }
+
+        $reports[$file][] = [
+            'line' => $message['line'],
+            'message' => $message['message'],
+            'type' => $message['type'],
+        ];
+
+        if ($message['type'] === 'ERROR') {
+            $hasError = true;
+        }
+    }
 }
 
-exit(1);
+
+if ($hasError) {
+    $output->writeln('<fg=red>Some styling errors were detected:</fg=red>');
+
+    $table = new Table($output);
+    $table->setHeaders(['File', 'Line', 'Type', 'Message']);
+
+    foreach ($reports as $file => $report) {
+        if (!$report) {
+            continue;
+        }
+
+        array_walk($report, function (array $data) use ($file, $table) {
+            $table->addRow([$file, $data['line'], $data['type'], $data['message']]);
+        });
+    }
+
+    $table->render();
+
+    exit(1);
+}
+
+$output->writeln('<info>No errors found! Congrats :)</info>');
